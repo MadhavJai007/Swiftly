ad542773cef95cadb650d01dd4c4e41a556ab9e9
Tobias Moktar <moktart@sheridancollege.ca>
2021-09-24 09:06:27 -0400

    Initial Commit

---
+++ Swiftly/SwiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyApp.swift
+//  Swiftly
+//
+//  Created by Toby moktar on 2021-09-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
