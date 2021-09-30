380016a32f5ece86fadf21e34a75bc33cfd83848
Tobias Moktar <moktart@sheridancollege.ca>
2021-09-30 10:54:08 -0400

    Added user type to the signup page.

--- Swiftly/Screens/SignupView/SignupView.swift
+++ Swiftly/Screens/SignupView/SignupView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 
 struct SignupView: View {
     
+    var userTypes = ["Student", "Teacher"]
+    @State private var selectedType = "Student"
+    
     @State private var placeholder = "Placeholder"
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
@@ -25,22 +28,22 @@ struct SignupView: View {
                     .ignoresSafeArea()
                 
                 VStack {
-                    
-                    Spacer()
-                    
+                
                     // Title
                     Text("Account Creation")
                         .font(.system(size: 75,design: .serif))
                         .foregroundColor(.white)
-                        .padding(.bottom, 25)
+                        .padding(.bottom, geometry.size.width/42)
+                        .padding(.top, -geometry.size.width/42)
                     
                     // Username
                     VStack(alignment: .leading){
                         
                         InputFieldLabel(text:"First Name")
+                            .padding(.bottom, -geometry.size.width/120)
                         
-                        SignupInputField(placeholderText: "Username", width: geometry.size.width - 150, height: 75, stateAttribute: placeholder)
-                    }.padding(.bottom, 25)
+                        SignupInputField(placeholderText: "Username", width: geometry.size.width - 150, height: geometry.size.width/12, stateAttribute: placeholder)
+                    }.padding(.bottom, geometry.size.width/42)
                     
                     
                     
@@ -50,19 +53,19 @@ struct SignupView: View {
                         VStack(alignment: .leading){
                            
                             InputFieldLabel(text:"First Name")
+                                .padding(.bottom, -geometry.size.width/120)
                             
-                            SignupInputField(placeholderText: "First Name", width: geometry.size.width/2 - 100, height: 75, stateAttribute: placeholder)
-                            
-                            
+                            SignupInputField(placeholderText: "First Name", width: geometry.size.width/2 - 100, height: geometry.size.width/12, stateAttribute: placeholder)
                         }
                         
                         VStack(alignment: .leading) {
                            
                             InputFieldLabel(text:"Last Name")
+                                .padding(.bottom, -geometry.size.width/120)
 
-                            SignupInputField(placeholderText: "Last Name", width: geometry.size.width/2 - 100, height: 75, stateAttribute: placeholder)
+                            SignupInputField(placeholderText: "Last Name", width: geometry.size.width/2 - 100, height: geometry.size.width/12, stateAttribute: placeholder)
                         }
-                    }.padding(.bottom, 25)
+                    }.padding(.bottom, geometry.size.width/42)
                     
                     // Country and DOB
                     HStack(spacing:geometry.size.width/18){
@@ -70,8 +73,9 @@ struct SignupView: View {
                         VStack(alignment: .leading){
                             
                             InputFieldLabel(text:"Country")
+                                .padding(.bottom, -geometry.size.width/120)
                             
-                            SignupInputField(placeholderText: "Country", width: geometry.size.width/2 - 100, height: 75, stateAttribute: placeholder)
+                            SignupInputField(placeholderText: "Country", width: geometry.size.width/2 - 100, height: geometry.size.width/12, stateAttribute: placeholder)
                             
                             
                         }
@@ -79,39 +83,64 @@ struct SignupView: View {
                         VStack(alignment: .leading) {
                             
                             InputFieldLabel(text:"Date of Birth")
+                                .padding(.bottom, -geometry.size.width/120)
                             
-                            SignupInputField(placeholderText: "Date of Birth", width: geometry.size.width/2 - 100, height: 75, stateAttribute: placeholder)
+                            SignupInputField(placeholderText: "Date of Birth", width: geometry.size.width/2 - 100, height: geometry.size.width/12, stateAttribute: placeholder)
                         }
-                    }.padding(.bottom, 25)
+                    }.padding(.bottom, geometry.size.width/42)
                     
                     // Email Address
                     VStack(alignment: .leading){
                         
                         InputFieldLabel(text:"Email Address")
+                            .padding(.bottom, -geometry.size.width/120)
                         
-                        SignupInputField(placeholderText: "Email Address", width: geometry.size.width - 150, height: 75, stateAttribute: placeholder)
+                        SignupInputField(placeholderText: "Email Address", width: geometry.size.width - 150, height: geometry.size.width/12, stateAttribute: placeholder)
                         
-                    }.padding(.bottom, 25)
+                    }.padding(.bottom, geometry.size.width/42)
                     
                     // Password
                     VStack(alignment: .leading){
                         
                         InputFieldLabel(text:"Password")
+                            .padding(.bottom, -geometry.size.width/120)
                         
-                        SignupInputField(placeholderText: "Password", width: geometry.size.width - 150, height: 75, stateAttribute: placeholder)
+                        SignupInputField(placeholderText: "Password", width: geometry.size.width - 150, height: geometry.size.width/12, stateAttribute: placeholder)
+                        
+                    }.padding(.bottom, geometry.size.width/42)
+                    
+                    HStack{
+                        
+                        InputFieldLabel(text:"User Type: ")
+                        
+                        
+                        Picker("", selection: $selectedType) {
+                                        ForEach(userTypes, id: \.self) {
+                                                       
+                                            Text($0)
+                                                .font(.system(size: 30))
+                                        }
+                                    }
+                        .frame(width: 100, height: 50)
+                        .background(Color.blackCustom)
+                        .cornerRadius(15)
+                        
                     }
 
-                    Spacer()
                     
                     // Create account button
                     Button{
                         self.mode.wrappedValue.dismiss()
                     }label:{
                         CreateAccountButton(text: "Create Account", textColor: .white, backgroundColor: Color.blackCustom)
-                            .padding(50)
+                            .padding(geometry.size.width/42)
                     }
                     
+                    
+                    
                 }
+                
+                Spacer()
             }
         }
             
